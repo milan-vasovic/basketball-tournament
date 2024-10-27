745a2fdc327680db87e2c661e1d2757ce5c82d75
MilanV <milan.vasovic.work@gmail.com>
2024-10-27 16:00:58 +0100

    default controller update

--- controllers/defaults.js
+++ controllers/defaults.js
@@ -2,7 +2,8 @@ const { loadDataFromFile, playersData } = require('../utils/load-data');
 
 exports.getDataFromFile = (req, res, next) => {
     try {
-        loadDataFromFile()
+        if (playersData.size < 1) {
+            loadDataFromFile()
             .then(() => {
                 res.status(200).json({ message: 'Data loaded successfully', players: Array.from(playersData.entries()) });
             })
@@ -10,6 +11,10 @@ exports.getDataFromFile = (req, res, next) => {
                 console.error('Error loading data:', err);
                 res.status(500).json({ msg: 'Error loading CSV data' });
             });
+        } else {
+            res.status(200).json({players: Array.from(playersData.entries()) });
+        }
+        
     } catch (err) {
         console.error('Unexpected error:', err);
         res.status(500).json({ msg: 'An error occurred while processing the request' });
